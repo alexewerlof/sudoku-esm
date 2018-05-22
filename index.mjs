c10595d6f68357e950223532e6b6d916556d2e88
Alex Ewerlöf <alex.ewerlof@schibsted.com>
2018-05-22 22:34:45 +0200

    show some stats

--- index.mjs
+++ index.mjs
@@ -4,14 +4,14 @@ import { solve } from "./lib/solver";
 const b = new Board();
 fillBoard(b,
   "956      ",
-  "437158269",
+  "         ",
   " 8  463 7",
   "6  28    ",
   "5  7  48 ",
   "2        ",
   "  5 7  4 ",
-  "8 1  372 ",
-  "729   6  "
+  "     372 ",
+  "      6  "
 );
 
 console.log(boardToString(b));
