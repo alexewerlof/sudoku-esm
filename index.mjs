0c022a3bf5ae45c5c9a57b9d5c72793060e3963f
Alex Ewerlöf <alex.ewerlof@schibsted.com>
2018-06-07 17:09:19 +0200

    some more optimizations

--- index.mjs
+++ index.mjs
@@ -1,17 +1,20 @@
-import { Board, boardToString, fillBoard } from "./lib/board";
-import { solve } from "./lib/solver";
+import { Board, boardToString, fillBoard } from './lib/board.mjs';
+import { solve } from './lib/solver.mjs';
 
 const b = new Board();
-fillBoard(b,
-  "956      ",
-  "        5",
-  " 8  463 7",
-  "6  28   1",
-  "5  7  48 ",
-  "2        ",
-  "  5 7  4 ",
-  "     372 ",
-  "      6  "
+fillBoard(
+  b,
+  '956      ',
+  '        5',
+  // ' 8  463 7',
+  // '6  28   1',
+  '         ',
+  '         ',
+  '5  7  48 ',
+  '2        ',
+  '  5 7  4 ',
+  '     372 ',
+  '      6  ',
 );
 
 console.log(boardToString(b));
