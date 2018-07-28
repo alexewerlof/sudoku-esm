bd9e19af81a22ff93487740c533394dad2a6edf0
Alex Ewerlöf <userpixel@users.noreply.github.com>
2018-07-29 00:53:58 +0200

    Improve speed and simplify the code (#1)

--- index.mjs
+++ index.mjs
@@ -1,17 +1,19 @@
-import { Board, boardToString, fillBoard } from "./lib/board";
-import { solve } from "./lib/solver";
+import { Board, boardToString } from './lib/board.mjs';
+import { solve } from './lib/solver.mjs';
 
 const b = new Board();
-fillBoard(b,
-  "956      ",
-  "         ",
-  " 8  463 7",
-  "6  28    ",
-  "5  7  48 ",
-  "2        ",
-  "  5 7  4 ",
-  "     372 ",
-  "      6  "
+b.fill(
+  '956      ',
+  '        5',
+  // ' 8  463 7',
+  // '6  28   1',
+  '         ',
+  '         ',
+  '5  7  48 ',
+  '2        ',
+  '  5 7  4 ',
+  '     372 ',
+  '      6  ',
 );
 
 console.log(boardToString(b));
