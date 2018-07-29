e998cc4d26f3668f1dfb8af650b76a82bdb153eb
Alex Ewerlöf <userpixel@users.noreply.github.com>
2018-07-30 01:27:59 +0200

    Set boundary (#5)
    * set a boundry for the solve function
    
    * set a boundry for the solve()

--- index.mjs
+++ index.mjs
@@ -1,19 +1,55 @@
 import { Board } from './lib/board.mjs';
 import { solve } from './lib/solver.mjs';
 
+const noSolution = '516849732' +
+'3 76 5   ' +
+'8 97   65' +
+'135 6 9 7' +
+'472591  6' +
+'96837  5 ' +
+'253186 74' +
+'6842 75  ' +
+'791 5 6 8'
+
+const oneSolution =  '95612 3 8' +
+'123578469' +
+'478369125' +
+'214635897' +
+'         ' +
+'3        ' +
+'531287946' +
+'847916532' +
+'692453781'
+
+const twoSolution =  '95612 3 8' +
+'123578469' +
+'478369125' +
+'214635897' +
+'         ' +
+'         ' +
+'531287946' +
+'847916532' +
+'692453781'
+
+const sixSolution =  '95612 3 8' +
+'123578469' +
+'478369125' +
+'214635897' +
+'         ' +
+'         ' +
+'531   946' +
+'847916532' +
+'692453781'
+
 const b = new Board(
-  '956      ' +
-  '        5' +
-  // ' 8  463 7' +
-  // '6  28   1' +
-  '         ' +
-  '         ' +
-  '5  7  48 ' +
-  '2        ' +
-  '  5 7  4 ' +
-  '     372 ' +
-  '      6  '
+  sixSolution
 );
 
-console.log(b.toString(b));
-solve(b);
+console.log('Starting with:')
+console.log(b.toString())
+console.time('computation');
+const answerBoards = solve(b, 3);
+console.timeEnd('computation')
+console.log(`Found ${answerBoards.length} answers:`)
+answerBoards.forEach(answerBoard => console.log(`${answerBoard}`))
+console.log('Done!')
