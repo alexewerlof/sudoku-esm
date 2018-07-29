600e3eacc0407008061bcdbe38e3ce418f0812f5
Alex Ewerlöf <userpixel@users.noreply.github.com>
2018-07-29 22:19:38 +0200

    copy the array instead of the diffBoard algorithm (#2)

--- index.mjs
+++ index.mjs
@@ -1,20 +1,19 @@
-import { Board, boardToString } from './lib/board.mjs';
+import { Board } from './lib/board.mjs';
 import { solve } from './lib/solver.mjs';
 
-const b = new Board();
-b.fill(
-  '956      ',
-  '        5',
-  // ' 8  463 7',
-  // '6  28   1',
-  '         ',
-  '         ',
-  '5  7  48 ',
-  '2        ',
-  '  5 7  4 ',
-  '     372 ',
-  '      6  ',
+const b = new Board(
+  '956      ' +
+  '        5' +
+  // ' 8  463 7' +
+  // '6  28   1' +
+  '         ' +
+  '         ' +
+  '5  7  48 ' +
+  '2        ' +
+  '  5 7  4 ' +
+  '     372 ' +
+  '      6  '
 );
 
-console.log(boardToString(b));
+console.log(b.toString(b));
 solve(b);
